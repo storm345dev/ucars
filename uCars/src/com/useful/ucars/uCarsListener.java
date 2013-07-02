3dff0800ba28cc159368b4dcfb35b4bfc44f75da
storm345 <storm345dev@gmail.com>
2013-07-02 16:02:12 +0100

    Realised that VehicleUpdateEvent isn't firing

--- uCars/src/com/useful/ucars/uCarsListener.java
+++ uCars/src/com/useful/ucars/uCarsListener.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
