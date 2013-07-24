cd98f497881827588f27432218d16661be3799e3
storm345 <storm345dev@gmail.com>
2013-07-24 13:41:40 +0100

    Made turrets take from your inventory

--- uCars/src/com/useful/ucars/MotionManager.java
+++ uCars/src/com/useful/ucars/MotionManager.java
@@ -1,12 +1,15 @@
 package com.useful.ucars;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.util.Vector;
@@ -37,7 +40,17 @@ public class MotionManager {
 				if(ucars.config.getBoolean("general.cars.turret")){
 				Vector arrowVel = plaD.clone();
 				arrowVel.setY(-0.01);
+				Boolean doArrow = true;
 				if(!player.hasMetadata("firing")){
+					if(player.getGameMode() != GameMode.CREATIVE){
+			    if(player.getInventory().contains(Material.ARROW)){
+			    player.getInventory().removeItem(new ItemStack(Material.ARROW,1));
+			    }
+			    else{
+			    	doArrow = false;
+			    }
+					}
+			    if(doArrow){
 				player.getWorld().spawnArrow(car.getLocation().add(0, 0.7, 0), arrowVel, 2, 1);
 				final String playername = player.getName();
 				player.setMetadata("firing", new FixedMetadataValue(ucars.plugin,true));
@@ -48,10 +61,13 @@ public class MotionManager {
 						Player p = ucars.plugin.getServer().getPlayer(playername);
 						if(p.hasMetadata("firing")){
 						p.removeMetadata("firing", ucars.plugin);
+						return;
 						}
 					}}, 10l);
+			    }
 				}
 				}
+				
 		    }
 			else if(side>0){//do right action
 				
