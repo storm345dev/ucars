e6fc9b4e6a449849f8a8871515c5b77598a50a6d
storm345 <storm345dev@gmail.com>
2015-02-19 17:18:37 +0000

    Make hitby more powerful

--- src/main/java/com/useful/ucars/uCarsListener.java
+++ src/main/java/com/useful/ucars/uCarsListener.java
@@ -1220,7 +1220,7 @@ public class uCarsListener implements Listener {
 		ent.removeMetadata("hitByLast", ucars.plugin);
 		ent.setMetadata("hitByLast", new StatValue(System.currentTimeMillis(), ucars.plugin));
 		
-		double speed = cart.getVelocity().length() * 0.1;
+		double speed = cart.getVelocity().length() * 0.15;
 		
 		double damage = crash_damage;
 		double pDmg = (damage * speed);
